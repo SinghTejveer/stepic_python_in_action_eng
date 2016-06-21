a57392aa716c341aa120c91050af9f5c0ab431d3
Pavel Karateev <karateev.pavel@ya.ru>
2016-06-21 13:28:38 +0300

    s120

---
+++ solutions/s120.py
@@ -0,0 +1,35 @@
+"""Interval
+
+Given an integer as input. Output True if its value is within the interval
+(−15,12]∪(14,17)∪[19,+∞), and False otherwise
+(case sensitive).
+
+Please note the different brackets, which are used to specify intervals.
+The problem uses semi-open and open intervals. You can read more about it on
+the Wikipedia.
+
+Sample Input 1:
+    20
+Sample Output 1:
+    True
+
+Sample Input 2:
+    -20
+Sample Output 2:
+    False
+"""
+
+
+def main():
+    num = int(input().rstrip())
+    if any([
+        -15 < num <= 12,
+        14 < num < 17,
+        19 <= num
+    ]):
+        print('True')
+    else:
+        print('False')
+
+if __name__ == '__main__':
+    main()
