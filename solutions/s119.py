4bfa4f1c186c1343da03824337560cc73eedec84
Pavel Karateev <karateev.pavel@ya.ru>
2016-06-21 13:23:24 +0300

    s119

---
+++ solutions/s119.py
@@ -0,0 +1,40 @@
+"""Triangle
+
+Given three natural numbers A, B, C. Define if the triangle with such sides
+exists.
+
+If the triangle exists - output the YES string, otherwise - output NO.
+
+Triangle is a three points that are not located on a single straight line.
+
+Sample Input:
+    3
+    4
+    5
+Sample Output:
+    YES
+"""
+
+
+def is_triangle(side1, side2, side3):
+    sides = [side1, side2, side3]
+
+    longest = max(side1, side2, side3)
+    shortest = min(side1, side2, side3)
+    middle = sum(sides) - longest - shortest
+
+    if middle > longest - shortest:
+        return 'YES'
+    else:
+        return 'NO'
+
+
+def main():
+    side1 = int(input().rstrip())
+    side2 = int(input().rstrip())
+    side3 = int(input().rstrip())
+
+    print(is_triangle(side1, side2, side3))
+
+if __name__ == '__main__':
+    main()
---
+++ tests/test_s119.py
@@ -0,0 +1,17 @@
+import pytest
+
+from solutions.s119 import is_triangle
+
+EXAMPLES = (
+    ('sides', 'expected'),
+    [
+        ([3, 4, 5], 'YES'),
+        ([3, 3, 3], 'YES'),
+        ([1, 2, 9], 'NO')
+    ]
+)
+
+
+@pytest.mark.parametrize(*EXAMPLES)
+def test_returns_correct_result(sides, expected):
+    assert is_triangle(*sides) == expected
