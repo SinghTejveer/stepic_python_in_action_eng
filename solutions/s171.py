9d7217f286a8aa86de8643432106f67e01d1326e
Pavel Karateev <karateev.pavel@ya.ru>
2016-07-03 20:01:28 +0300

    s171

---
+++ solutions/s171.py
@@ -0,0 +1,53 @@
+"""
+You should write a program that can transform some units of measurement into
+others.
+
+The following transformations should be supported:
+
+    miles (1 mile = 1609 m),
+    yards (1 yard = 0.9144 m),
+    feet (1 foot = 30.48 cm),
+    inches (1 inch = 2.54 cm),
+    kilometres (1 km = 1000 m),
+    meters (m),
+    centimetres (1 cm = 0.01 m)
+    millimetres (1 mm = 0.001 m)
+
+Use the units of measurement specified in the problem description with the
+exact specified accuracy.
+
+Input format:
+Single line in the following format:
+<number> <unit_from> in <unit_to>
+For example: if you get the line "15.5 mile in km", then you should transform
+15.5 miles into kilometers.
+
+Output format:
+Real number in scientific format (exponential), with an accuracy of exactly
+two digits after decimal point.
+"""
+
+
+TO_METERS = {
+    'mile': 1609,
+    'yard': 0.9144,
+    'foot': 0.3048,
+    'inch': 0.0254,
+    'km': 1000,
+    'm': 1,
+    'cm': 0.01,
+    'mm': 0.001
+}
+
+
+def solve(command: str) -> str:
+    value, base, _, to = command.split()
+    return '%.2e' % (float(value)*TO_METERS[base]/TO_METERS[to])
+
+
+def main():
+    command = input().rstrip()
+    print(solve(command))
+
+if __name__ == '__main__':
+    main()
---
+++ tests/test_s171.py
@@ -0,0 +1,16 @@
+import pytest
+
+from solutions.s171 import solve
+
+
+EXAMPLES = (
+    ('command', 'expected'),
+    [
+        ('15.5 mile in km', '2.49e+01'),
+    ]
+)
+
+
+@pytest.mark.parametrize(*EXAMPLES)
+def test_returns_correct_result(command, expected):
+    assert solve(command) == expected
