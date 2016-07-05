cd9cebef68fe79ef4fd2edfaf25ca00d246254fb
Pavel Karateev <karateev.pavel@ya.ru>
2016-07-05 14:52:26 +0300

    s180

---
+++ solutions/s180.py
@@ -0,0 +1,16 @@
+"""
+Write a program, which reads the line from a standard input, using the input()
+function, and outputs the same line to the standard output, using the print()
+function.
+
+Please pay attention that you need to use the input function without any
+parameters, and that you need to output only that, what was transferred
+to the input of this program.
+"""
+
+
+def main():
+    print(input())
+
+if __name__ == '__main__':
+    main()
