0fabe45a7b0e13eb737f7d030337e560b17b5474
Pavel Karateev <karateev.pavel@ya.ru>
2017-04-24 18:07:47 +0300

    Removes needless venv activation check

--- tasks.py
+++ tasks.py
@@ -17,21 +17,11 @@ def render_template(template, context):
 
 
 @task
-def check_venv(ctx):
-    # http://stackoverflow.com/a/1883251/2787185
-    # Is virtual environment active?
-    pyenv = os.environ.get('PYENV_VIRTUAL_ENV', False)
-    if not hasattr(sys, 'real_prefix') and not pyenv:
-        print('Activate a virtual environment before!')
-        sys.exit()
-
-
-@task(check_venv)
 def tests(ctx):
     ctx.run('python -m pytest --doctest-modules')
 
 
-@task(check_venv)
+@task
 def syntax(ctx):
     ctx.run('python -m pylint solutions utils tests')
 
